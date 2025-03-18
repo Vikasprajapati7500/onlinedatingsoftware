ca1a5c4cf88dedeeaf9d979d82d1d3eaf763d419
Vikas kumar <vk186272@gmail.com>
2025-03-18 13:03:25 +0530

    Health consultation

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,72 +1,72 @@
-import { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import  prisma  from "./prisma";
-import { compare } from "bcryptjs";
+// import { NextAuthOptions } from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import  prisma  from "./prisma";
+// import { compare } from "bcryptjs";
 
-export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    secret: process.env.NEXTAUTH_SECRET,
-    session: {
-        strategy: "jwt",
-    },
-    pages: {
-        signIn: "/admin/sign-in",
-    },
-    providers: [
-        CredentialsProvider({
-            // The name to display on the sign in form (e.g. "Sign in with...")
-            name: "Credentials",
+// export const authOptions: NextAuthOptions = {
+//     adapter: PrismaAdapter(prisma),
+//     secret: process.env.NEXTAUTH_SECRET,
+//     session: {
+//         strategy: "jwt",
+//     },
+//     pages: {
+//         signIn: "/admin/sign-in",
+//     },
+//     providers: [
+//         CredentialsProvider({
+//             // The name to display on the sign in form (e.g. "Sign in with...")
+//             name: "Credentials",
             
-            credentials: {
-                email: { label: "Email", type: "email", placeholder: "jsmith" },
-                password: { label: "Password", type: "password" },
-            },
-            async authorize(credentials) {
-                // Add logic here to look up the user from the credentials supplied
-                if (!credentials?.email || !credentials?.password) {
-                    return null;
-                }
+//             credentials: {
+//                 email: { label: "Email", type: "email", placeholder: "jsmith" },
+//                 password: { label: "Password", type: "password" },
+//             },
+//             async authorize(credentials) {
+//                 // Add logic here to look up the user from the credentials supplied
+//                 if (!credentials?.email || !credentials?.password) {
+//                     return null;
+//                 }
 
-                const existingUser = await prisma.user.findUnique({
-                    where: {
-                        email: credentials.email,
-                    },
-                });
-                if (!existingUser) {
-                    return null;
-                }
+//                 const existingUser = await prisma.user.findUnique({
+//                     where: {
+//                         email: credentials.email,
+//                     },
+//                 });
+//                 if (!existingUser) {
+//                     return null;
+//                 }
 
-                const passwordMatch = existingUser.password && (await compare(credentials.password, existingUser.password));
-                if (!passwordMatch) {
-                    return null;
-                }
-                return {
-                    id: `${existingUser.id}`,
-                    // username: existingUser.username,
-                    email: existingUser.email,
-                };
-            },
-        }),
-    ],
-    callbacks: {
-        async jwt({ token, user }) {
-            if (user) {
-                return {
-                    ...token,
-                    // username: user.username,
-                };
-            }
-            return token;
-        },
-        async session({ session, user, token }) {
-            return {
-                ...session,
-                user: {
-                    ...session.user,
-                    username: token.username,
-                },
-            };
-        },
-    },
-};
+//                 const passwordMatch = existingUser.password && (await compare(credentials.password, existingUser.password));
+//                 if (!passwordMatch) {
+//                     return null;
+//                 }
+//                 return {
+//                     id: `${existingUser.id}`,
+//                     // username: existingUser.username,
+//                     email: existingUser.email,
+//                 };
+//             },
+//         }),
+//     ],
+//     callbacks: {
+//         async jwt({ token, user }) {
+//             if (user) {
+//                 return {
+//                     ...token,
+//                     // username: user.username,
+//                 };
+//             }
+//             return token;
+//         },
+//         async session({ session, user, token }) {
+//             return {
+//                 ...session,
+//                 user: {
+//                     ...session.user,
+//                     username: token.username,
+//                 },
+//             };
+//         },
+//     },
+// };
